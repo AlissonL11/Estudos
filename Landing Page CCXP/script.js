42d5d8776fd4c0142f5f1b7d9ceb513b20ec9a17
AlissonL11 <alisson.lima11@outlook.com.br>
2023-01-06 15:40:44 -0300

    Adicionando alterações com base nas aulas gravadas assistidas

--- Landing Page CCXP/script.js
+++ Landing Page CCXP/script.js
@@ -30,7 +30,74 @@ function formatoTempo(tempo) {
 countDown()
 setInterval(countDown, 1000)
 
+// fim do countdown //
+
+function checkKeyboardCode() {
+    document.addEventListener('keydown', (event) =>{
+        var name = event.key;
+        var code = event.code;
+        alert(`tecla pressionada ${name} \r \n Código da tecla ${code}`);
+    },false);
+}
+//checkKeyboardCode();
+
+//fim da função que verifica a tecla pressionada
+
+function addKeyboardEventListeners() {
+
+    document.addEventListener('keydown', (event) => {
+
+        var ingresso1 = document.getElementById("quinta");
+        var ingresso2 = document.getElementById("sexta");
+        var ingresso3 = document.getElementById("sabado");
+        var ingresso4 = document.getElementById("domingo");
+
+        var code = event.code;
+            if(code == "Digit1") {
+                ingresso1.classList.toggle('card-highlight');
+                ingresso2.classList.remove('card-highlight');
+                ingresso3.classList.remove('card-highlight');
+                ingresso4.classList.remove('card-highlight');
+            }
+            if(code == 'Digit2') {
+                ingresso1.classList.remove('card-highlight');
+                ingresso2.classList.toggle('card-highlight');
+                ingresso3.classList.remove('card-highlight');
+                ingresso4.classList.remove('card-highlight');
+            }
+            if(code == 'Digit3') {
+                ingresso1.classList.remove('card-highlight');
+                ingresso2.classList.remove('card-highlight');
+                ingresso3.classList.toggle('card-highlight');
+                ingresso4.classList.remove('card-highlight');
+            }
+            if(code == 'Digit4') {
+                ingresso1.classList.remove('card-highlight');
+                ingresso2.classList.remove('card-highlight');
+                ingresso3.classList.remove('card-highlight');
+                ingresso4.classList.toggle('card-highlight');
+            }
+    }, false);
+}
+
+addKeyboardEventListeners();
+
+// fim da função para selecionar os ingressos pelo teclado //
+
+const ingressos = [];
+
 function highlightCard(selector) {
     var element = document.querySelector(selector);
     element.classList.toggle('card-highlight')
+}
+
+function selectCard(selector) {
+    var element = document.querySelector(selector);
+    element.classList.toggle("card-selected");
+        if(ingressos.includes(selector)) ingressos.pop(selector);
+        else ingressos.push(selector)
+}
+
+function showSelectedCards() {
+    if(ingressos.length > 0) alert("Ingressos Selecionados: " + ingressos);
 }
