91d15f0ec811c49afc7b237140a751e4d087e075
AlissonL11 <alisson.lima11@outlook.com.br>
2023-01-09 23:12:46 -0300

    Finalizadas as questões de erro no JS, página concluída

--- ByeCar/script.js
+++ ByeCar/script.js
@@ -1,6 +1,8 @@
 // Define o índice do item atual
 let currentIndex = 0;
 
+let timer; //variável para o temporizador
+
 // Seleciona o elemento que contém a LISTA de cards
 const carouselCards = document.querySelector("#carousel-cards");
 
@@ -31,32 +33,67 @@ function prev() {
   items[currentIndex].style.display = "block";
 }
 
-// Mudando de item automaticamente
+// Mudando de item automaticamente, a cada 5 segundos
 
-setInterval(next, 3000)
+timer = setInterval(next, 5000)
 
 
-// modais (faltando associar o modal ao item certo a ser exibido)
+function pausar(){        //funçao criada para conseguir pausar o giro dos cards, ao clicar em "saiba mais"
+  clearInterval(timer);
+}
+function continuar(){      //continua o giro automático do carrossel
+  timer = setInterval(next, 5000)
+}
+
+
+
+// modais ABAIXO !!!
 
 function exibir() {
-    if (currentIndex = 0) {
-        var modal = document.getElementById("modal-1");
-    if (modal.style.display === "block") {
+
+    pausar(); //chamando a função, para pausar o giro do carrossel enquanto as informações extras são exibidas
+
+
+    if (currentIndex == 0) {                      //verifica qual o index atual (card atual sendo exibido)
+        var modal = document.getElementById("modal-1"); //pega o id do modal correspondente ao card (index)
+    if (modal.style.display === "block") {            //faz o display ser alterado de none para block (as duas ultimas linhas são basicamente isso)
       modal.style.display = "none";
     } else {
       modal.style.display = "block";
     }
     };
 
-    if (currentIndex = 1){
+    if (currentIndex == 1){
     var modal = document.getElementById("modal-2");
     if (modal.style.display === "block") {
       modal.style.display = "none";
+
+      clearInterval(timer);
     } else {
       modal.style.display = "block";
     }
   }
 }
 
+function fechar(){
+  continuar()     //continua o giro do carrossel
 
-  
+  if (currentIndex == 0) {                            //essa função é basicamente o inverso daquela lá em cima, que exibe os modais.
+    var modal = document.getElementById("modal-1");
+if (modal.style.display == "none") {
+  modal.style.display == "block";
+} else {
+  modal.style.display = "none";
+}
+};
+
+if (currentIndex == 1){
+var modal = document.getElementById("modal-2");
+if (modal.style.display === "none") {
+  modal.style.display = "block";
+} else {
+  modal.style.display = "none";
+}
+}
+}
+
